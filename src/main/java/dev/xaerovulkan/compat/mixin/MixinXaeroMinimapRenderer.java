c900795a3704af18c676f3d3770749a7fc75a348
lozampy <carlo.zampieri3@gmail.com>
2026-04-20 16:47:05 +0200

    Fix import statement for MatrixStack

--- src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroMinimapRenderer.java
+++ src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroMinimapRenderer.java
@@ -2,7 +2,7 @@ package dev.xaerovulkan.compat.mixin;
 
 import dev.xaerovulkan.compat.XaeroVulkanCompat;
 import dev.xaerovulkan.compat.render.VulkanCompatRenderManager;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
